adc26d9cd88436982881ae65aada54e8bea5d0c5
Saphal Ghimire <saphal.ghimire.sg@gmail.com>
2025-09-16 20:56:33 +0545

    try

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,18 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, HashRouter } from "react-router-dom";
 import './index.css'
 import App from './App.tsx'
 
+
 createRoot(document.getElementById('root')!).render(
+  <HashRouter>
  <StrictMode>
     <BrowserRouter basename="/react/">
       <Routes>
         <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
-  </StrictMode>,
+  </StrictMode>
+  </HashRouter>,
 )
